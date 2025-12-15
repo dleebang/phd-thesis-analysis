408c039184ced35cbe04edeac1e8c19b0a2a8fa8
Davi Lee Bang <81483121+dleebang@users.noreply.github.com>
2025-12-15 10:25:36 -0300

    Update analysis_phyPCA.R

--- analysis_phyPCA.R
+++ analysis_phyPCA.R
@@ -1,15 +1,48 @@
-#select cont. variables
-data_model <- data %>% select(call_duration, call_rate, call_freq_pk, call_freq_bw, mean_svl)
-#z-scale data
-data_model <- scale(data_model)
-#build a treedata object and extract data with species names for each variable
-data_model <- treedata(tree, data_model) %>% .$data
+  ultra.tree <- ape::read.tree('bok_ultra.tre')
+  ggplot2::theme_set(ggplot2::theme_bw())
+  
+  #select cont. variables
+  data_model <- data %>% select(larynx_size:constrictor_post_pcsa) %>% log() %>% scale()
 
-data_model <- data_model[-28,]
-pca <- prcomp(data_model)
+  #build a treedata object and extract data with species names for each variable
+  data_model <- treedata(ultra.tree, data_model) %>% .$data
 
-plot(pca$x[,"PC1"], pca$x[,"PC2"])
 
-PC <- as.data.frame(pca$x)
+pca <- phyl.pca(ultra.tree, data_model, method = "BM")
+plot(pca)
+biplot(pca)
+plot(pca$Evec[,'PC1'], pca$Evec[,'PC2'])
+
+PC <- as.data.frame(pca$Evec)
 ggplot(aes(x = PC[,"PC1"], y = PC[,"PC2"], color = rownames(PC)), data = PC) +
   geom_point()
+
+
+
+
+## EXPLORE PCA
+# note: normal pca will be applied to residuals of anatomical traits vs svl
+# in this case, phylogeny is already taken into account since we used pgls
+
+# get residuals from anatomic pgls analysis, this time taking muscle vol and fiber length into acc as well
+residuals_an <- residuals
+
+# merge residuals with discrete acoustic traits
+data_ac <- data[,c('structure', 'emission_pattern', 'note_types')]
+
+data_ac['structure'] <- ifelse(data_ac['structure'] == 0, 'Tonal', 'Pulsado') %>% as.factor()
+data_ac['emission_pattern'] <- ifelse(data_ac['emission_pattern'] == 0, 'Continuo', 'Serie') %>% as.factor()
+data_ac['note_types'] <- case_when(data_ac['note_types'] == 0 ~ 'Um', 
+                                   data_ac['note_types'] == 1 ~ 'Dois_obrigatorio',
+                                   data_ac['note_types'] == 2 ~ 'Dois_facultativo') %>% as.factor()
+
+data_pca <- cbind(residuals_an, data_ac)
+
+PC <- prcomp(data_pca[,1:14])
+PC <- as.data.frame(PC$x)
+
+ggplot(aes(PC1, PC2, label = rownames(PC), color = data_pca[,'structure']), data = PC) + geom_label(size = 3)
+
+ggplot(aes(PC1, PC2, label =  rownames(PC), color = data_pca[,'emission_pattern']), data = PC) + geom_label(size = 3)
+
+ggplot(aes(PC1, PC2, label =  rownames(PC), color = data_pca[,'note_types']), data = PC) + geom_label(size = 3)
